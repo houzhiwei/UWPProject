6abc8368e28f68869a5afa08c0dd7b9318840f0a
zack.hou <zack.hou@myviewboard.com>
2022-07-05 17:24:26 +0800

    UWP

---
+++ DragDropTestApp/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace DragDropTestApp
+{
+	public class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<PreviewItem> PreviewItems = new ObservableCollection<PreviewItem>();
+        public MainViewModel()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                var item = new PreviewItem();
+                item.ImageThumbnail = new Windows.UI.Xaml.Controls.Image();
+                ImageSource result = new BitmapImage(new Uri("ms-appx:///Assets/Square150x150Logo.png"));
+                item.ImageThumbnail.Source = result;
+
+                PreviewItems.Add(item);
+            }
+        }
+    }
+}
---
+++ DragDropTestApp/PreviewItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace DragDropTestApp
+{
+	public class PreviewItem : ObservableObject
+    {
+        private Image _imageThumbnail;
+        public Image ImageThumbnail { get => _imageThumbnail; set => SetProperty(ref _imageThumbnail, value); }
+
+
+        public PreviewItem()
+        {
+
+        }
+
+    }
+}
